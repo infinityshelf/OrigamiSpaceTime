23698bcc263ea530d56c3e27d413f383b60e7864
Estevan Hernandez <estevan@bluerocket.us>
2017-08-05 14:10:51 -0700

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Estevan Hernandez on 8/4/17.
+//
+
+#include <iostream>
+#include "Game.hpp"
+
+int main(int argc, char const *argv[]) {
+    std::cout << "Hello Origami Space Time!" << std::endl;
+    Game::main(argc, argv);
+    return 0;
+}
